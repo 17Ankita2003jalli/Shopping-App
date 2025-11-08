61b834f41648d11eaee9b5ad7cce646844c91955
Ankita <ankitajalli17@gmail.com>
2025-11-08 19:40:01 +0530

    category modified files

--- ecommerce-backend/src/main/java/com/ecom/ecommerce_backend/repository/CategoryRepo.java
+++ ecommerce-backend/src/main/java/com/ecom/ecommerce_backend/repository/CategoryRepo.java
@@ -1,10 +1,20 @@
 package com.ecom.ecommerce_backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ecom.ecommerce_backend.model.Category;
 @Repository
 public interface CategoryRepo extends JpaRepository<Category, Long> {
+	@Query("SELECT c FROM Category c LEFT JOIN FETCH c.products WHERE c.id = :id")
+	Optional<Category> findByIdWithProducts(@Param("id") Long id);
+
+	@Query("SELECT c FROM Category c LEFT JOIN FETCH c.products")
+	List<Category> findAllWithProducts();
 
 }
